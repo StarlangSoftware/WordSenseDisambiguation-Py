71b567b4994cb8c1bc6e905fc66d846f949c964a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 12:24:09 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-WordSenseDisambiguation',
+    version='1.0.0',
+    packages=['WordSenseDisambiguation', 'WordSenseDisambiguation.Sentence', 'WordSenseDisambiguation.ParseTree'],
+    url='https://github.com/StarlangSoftware/WordSenseDisambiguation-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Word Sense Disambiguation Library'
+)
