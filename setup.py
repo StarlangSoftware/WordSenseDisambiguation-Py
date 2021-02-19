14bf565f7e3400138e5386e99ed0aa8841496387
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-02-19 15:30:10 +0300

    Updated lesk, most frequent and random base lines.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-WordSenseDisambiguation',
-    version='1.0.0',
+    version='1.0.1',
     packages=['WordSenseDisambiguation', 'WordSenseDisambiguation.Sentence', 'WordSenseDisambiguation.ParseTree'],
     url='https://github.com/StarlangSoftware/WordSenseDisambiguation-Py',
     license='',
